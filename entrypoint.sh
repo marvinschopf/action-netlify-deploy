7a00a716779c2b0eaf2aa114d3622e721e1f2480
Marvin <marvin@schopf.biz>
2021-02-20 17:07:14 +0100

    Do not interfere in other people's affairs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-npm i -g netlify-cli
-
 NETLIFY_AUTH_TOKEN=$1
 NETLIFY_SITE_ID=$2
 NETLIFY_DEPLOY_TO_PROD=$3
@@ -11,17 +9,11 @@ INSTALL_COMMAND=$6
 BUILD_COMMAND=$7
 DEPLOY_ALIAS=$8
 
-# Install dependencies
-eval ${INSTALL_COMMAND:-"npm i"}
-
-# Build project
-eval ${BUILD_COMMAND:-"npm run build"}
-
 # Export token to use with netlify's cli
 export NETLIFY_SITE_ID=$NETLIFY_SITE_ID
 export NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN
 
-COMMAND="netlify deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message=\"$INPUT_NETLIFY_DEPLOY_MESSAGE\""
+COMMAND="npx netlify-cli deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message=\"$INPUT_NETLIFY_DEPLOY_MESSAGE\""
 
 if [[ $NETLIFY_DEPLOY_TO_PROD == "true" ]]
 then
